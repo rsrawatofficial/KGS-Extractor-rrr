4839a3820cb1ff24b3c74e8a5aab36d401c3ab41
rsrawatofficial <rsrasasingh@gmail.com>
2025-05-04 19:28:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 28228907
-api_hash = "3848da16c807890cabc8d1395248c9cf3"
-bot_token = "6945259945:AAEG0hlngK-ORA4VrwZ6GxwybN8ei4H80jDwI"
-auth_users = "6127558921"
+api_id = 27900743
+api_hash = "ebb06ea8d41420e60b29140dcee902fc"
+bot_token = "8003649544:AAGoiThVN8KLJyKGsGf1BcfTsjDTrSmjFR8"
+auth_users = "7804396225"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6127558921"
+osowner_users = "7804396225"
 owner_users = [int(num) for num in osowner_users.split(",")]
